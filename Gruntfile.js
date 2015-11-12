5587346575db3ec3225d080b9c236bdab3d41afd
VladimirStrelok <alexander.glica@gmail.com>
2015-11-12 13:53:14 -0500

    x8 v2

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+
+module.exports = function (grunt) {
+
+
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-jade");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks('grunt-bowercopy');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build:{
+        src: [
+          'public_html/js/**/*.js',
+          'public_html/css/**/*.css',
+          'public_html/**/*.html',
+          'public_html/fonts/**/*.*',
+          '!public_html/.gitkeep'
+        ]
+      }
+    },
+    less: {
+     dev: {
+       src:"less/style.less",
+       dest:"public_html/css/style.css"
+     },
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      build: {
+        src: ['scripts/angular/**/*.js'],
+        dest: 'public_html/js/<%= pkg.name %>.js'
+      }
+    },
+    jade: {
+      build: {
+        options: {
+          pretty: true,
+          data: {
+            pkgName: "<%= pkg.name %>"
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: 'jade',
+          src: ['**/*.jade'],
+          dest: './public_html',
+          ext: '.html'
+        }]
+      }
+    },
+    bowercopy: {
+      options: {
+        srcPrefix: 'bower_components'
+      },
+      build:{
+        options: {
+          destPrefix: 'public_html/js/vendor'
+        },
+        files: {
+          'angular/angular.js': 'angular/angular.min.js',
+          'trianglify/trianglify.js': 'trianglify/dist/trianglify.min.js'
+        }
+      }
+    },
+    copy:{
+      fonts: {
+        expand: true,
+        cwd: 'bower_components/font-awesome/fonts/',
+        src: '**',
+        dest: 'public_html/fonts',
+        flatten:true
+      },
+      img: {
+        expand: true,
+        cwd: 'img',
+        src: '**',
+        dest: 'public_html/img'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['clean', 'jade', 'bowercopy', 'copy','less','concat']);
+
+  grunt.registerTask("dev", ['clean:build','less:dev','jade:dev','bowercopy:dev','concat:dev',
+                              'copy:dev'])
+};
---
+++ scripts/angular/body.js
@@ -0,0 +1,4 @@
+angular.module('app.body', [])
+.controller("BodyCtrl",[function(){
+  
+}])
